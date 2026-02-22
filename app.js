d6f5407e5a28e81a7be1bbe90b1e94b83a145bef
crzydv <crzydv00@gmail.com>
2026-02-22 11:18:55 +0530

    write

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ fs.readFile("file.txt", "utf8", function (err, data) {
   console.log(data);
 });
 console.log("readFile");
+const fs = require("fs");
+fs.writeFile("file.txt", "Hello World!", function (err) {
+  if (err) throw err;
+  console.log("File saved!");
+});
+console.log("writeFile");
