c67028bf37e5ef46171101e821f9393888429461
crzydv <crzydv00@gmail.com>
2026-02-22 11:24:33 +0530

    web

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ fs.readFile("file.txt", "utf8", function (err, data) {
   console.log(data);
 });
 console.log("readFile");
-const fs = require("fs");
 fs.writeFile("file.txt", "Hello World!", function (err) {
   if (err) throw err;
   console.log("File saved!");
 });
 console.log("writeFile");
+const http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+res.write('Hello World!');
+res.end();
+}).listen(8080);
+console.log("webServer");
